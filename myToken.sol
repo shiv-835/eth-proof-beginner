d3f618a665dc4bd7ef63fc721464818fd4a6ee35
Shiv Chauhan <shiv.chauhan1518@gmail.com>
2024-06-12 01:50:15 +0530

    Update myToken.sol

--- myToken.sol
+++ myToken.sol
@@ -4,23 +4,23 @@ pragma solidity ^0.8.5;
 contract MyToken {
 
     // public variables
-    string public tokenname = "Squish";
-    string public tokenabbreviation = "sqh";
-    uint public totalsupply = 0;
+    string public tokenName = "Squish";
+    string public tokenAbbrev = "sqh";
+    uint public totalSupply = 0;
 
     // mapping variable
     mapping(address => uint) public balances;
 
     // mint function
     function mint(address _address,uint _value) public {
-        totalsupply += _value;
+        totalSupply += _value;
         balances[_address] += _value;
     }
 
     // burn function
     function burn(address _address,uint _value) public {
         if (balances[_address] >= _value){
-            totalsupply -= _value;
+            totalSupply -= _value;
             balances[_address] -= _value;
         }
     }
